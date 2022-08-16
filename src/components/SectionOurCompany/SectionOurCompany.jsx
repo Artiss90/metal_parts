2c2f951654e7d8dc1b7e0e7c54cf702c7ccb8f02
Artem <64410376+Artiss90@users.noreply.github.com>
2022-08-16 22:19:13 +0300

    translate section OurCompany

--- src/components/SectionOurCompany/SectionOurCompany.jsx
+++ src/components/SectionOurCompany/SectionOurCompany.jsx
@@ -2,6 +2,7 @@ import CartGroupService from 'components/CartGroupService/CartGroupService';
 import TitleBySection from 'components/common/TitleBySection/TitleBySection';
 import img1 from 'img/jpg/Rectangle 25company1.jpg';
 import img2 from 'img/jpg/Rectangle 25company2.jpg';
+import { FormattedMessage } from 'react-intl';
 
 import style from './SectionOurCompany.module.css';
 
@@ -10,7 +11,9 @@ function SectionOurCompany() {
     [
       img1,
       'Почему мы?',
+      'header.ourCompany.whyUs',
       'Люди – профессионалы с большим опытом работы. Полный спектр работ по металлообработке в одном месте – комплексный индивидуальный подход. Конструкторский отдел. Логистика. Многолетний опыт машиностроения – создание промышленного',
+      'header.ourCompany.whyUs_description',
     ],
     [
       img2,
@@ -22,7 +25,10 @@ function SectionOurCompany() {
     <section className={style.container} id="OurCompany">
       {/* //! replace inlineStyle*/}
       <TitleBySection inlineStyle={{ marginBottom: '30px' }}>
-        Наша компания
+        <FormattedMessage
+          id="header.ourCompany"
+          defaultMessage="Наша компания"
+        />
       </TitleBySection>
       <CartGroupService groupImg={imgGroup} col={1} />
     </section>
